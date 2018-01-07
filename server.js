a6a51ff0bcaf85e205b4aa707e55158e86852ed7
dudd1ts <kserg90@gmail.com>
2018-01-07 17:37:23 +0300

    Basic work completed

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+// var io = require('../..')(server);
+var io = require('socket.io')(server);
 
-app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/index.html');
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function (socket) {
+    var user = Date.now();
+
+    socket.on('message.sent', function (message) {
+        io.emit('message', user + ': ' + message);
+    });
+
+    io.emit('message ', 'User' + user + ' connected');
 });
 
-http.listen(3000, function () {
+server.listen(3000, function () {
     console.log('Started server');
 });
